50d30533bc40321a4900a09066607b09b5de286c
Bartosz Kaliszuk <bartosz.kaliszuk@gmail.com>
2015-02-27 08:20:53 +0100

    implement encoder
    First encoder approach. Based on idea of ascii codes for letters as
    offsets.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,31 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (:require [clojure.string :as str]))
+
+(def letters
+  "Alphabet used later on for decoding/encoding of the message"
+  (map char (range (int \a) (inc (int \z)))))
+
+(defn full-cipher
+  "Return full cipher - cipher repeated till message length is met."
+  [cipher message]
+  (let [cipher-len (count cipher)
+        msg-len (count message)
+        to-take (Math/ceil (/ msg-len cipher-len))]
+    (take msg-len (apply concat (take to-take (repeat cipher))))))
+
+(defn- encode-letters [k m]
+  (let [offset 97 ;; letter 'a' offset
+        index (- (int k) offset)
+        split-pos (- (int m) offset)]
+    (nth (apply concat (reverse (split-at split-pos letters))) index)
+    ;;[k index]
+    ))
 
 (defn encode [keyword message]
-  "encodeme")
+  (let [cipher (apply str (full-cipher keyword message))
+        msg (str/lower-case message)]
+    (apply str (map encode-letters cipher msg))))
 
 (defn decode [keyword message]
   "decodeme")
+
